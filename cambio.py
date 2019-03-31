f06f7a1c4696e8b059d39ab7c30d434ffd33c5ea
Anderson Nogueira <almnogueira1976@gmail.com>
2019-03-31 15:30:17 -0300

    Gerando arquivo CSV

--- cambio.py
+++ cambio.py
@@ -1,7 +1,9 @@
 #Teste de programa para acessar uma url
 
+import math
 import requests # importando a biblioteca de acessos url
 import json
+import pandas as pd #importar a biblioteca pandas, dando o alias (apelido) pd
 
 url="http://data.fixer.io/api/latest?access_key=27434bfe92edde6a5a5d68303ef4b443"
 
@@ -14,22 +16,37 @@ if response.status_code == 200: # "status.code é uma resposta de uma lista"
 
     dados = response.json() #todo json é um dicionário
     day = dados['date']
-    print("Acessando dados do dia %s / %s / %s" % (day[8:],day[5:7],day[0:4]))
+    print("Acessando dados do dia %s/%s/%s" % (day[8:],day[5:7],day[0:4]))
 
-    print("Valor do Euro.: %.2f" % dados['rates']['EUR'])
-    print("Valor do Real.: %.2f" % dados['rates']['BRL'])
-    print("Valor do Dolar.: %.2f" % dados['rates']['USD'])
-    print("Valor do BitCoin.: ", dados['rates']['BTC'])
+#    print("Valor do Euro.: %.2f" % dados['rates']['EUR'])
+#    print("Valor do Real.: %.2f" % dados['rates']['BRL'])
+#    print("Valor do Dolar.: %.2f" % dados['rates']['USD'])
+#    print("Valor do BitCoin.: ", dados['rates']['BTC'])
 
-    euro_real = dados['rates']['BRL'] / dados['rates']['EUR']
-    dollar_real = dados['rates']['BRL'] / dados['rates']['USD']
-    btc_real = dados['rates']['BRL'] / dados['rates']['BTC']
+    euro_real = round(dados['rates']['BRL'] / dados['rates']['EUR'], 2)
+    dollar_real = round(dados['rates']['BRL'] / dados['rates']['USD'], 2)
+    btc_real = round(dados['rates']['BRL'] / dados['rates']['BTC'], 2)
 
     print("Convertendo ....")
 
     print("%.2f" % euro_real)
     print("%.2f" % dollar_real)
     print("%.2f" % btc_real)
+
+    #criando a tabela de dados
+    df = pd.DataFrame({'Moedas': ['Euro', 'Dollar', 'Bitcoin'], 'Valores':[euro_real, dollar_real, btc_real]})
+    #exportando para csv - arquivo de tabelas que o excel entende, sem coluna de indice e
+    #utilizando o ";" como separador das colunas
+
+    #Se quiser dar um nome ao índice
+    #df.index.name = "Item" 
+    print("Gerando Arquivo CSV....")
+
+    df.to_csv("valores.csv", index=False, sep=";") #aqui ele desconsidera a colona de índice
+
+    print("Arquivo CSV gerado com SUCESSO !!!")
+
+    
 else:
     print("Error na conexão !!!")
 
